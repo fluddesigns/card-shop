16dc2245a4560b23562fb32ff07d1cb948d17c7f
fluddesigns <blakeflud@gmail.com>
2025-12-16 13:20:02 -0600

    change default sort to a-z

--- app.py
+++ app.py
@@ -145,7 +145,7 @@ def profile():
 def user_storefront(username):
     user = User.query.filter_by(username=username.lower()).first_or_404()
     settings = get_user_settings(user.id)
-    inventory = Card.query.filter_by(user_id=user.id).filter(Card.quantity > 0).order_by(Card.price.desc()).all()
+    inventory = Card.query.filter_by(user_id=user.id).filter(Card.quantity > 0).order_by(Card.card_name.desc()).all()
     return render_template('index.html', inventory=inventory, show_prices=settings.show_prices, owner=user)
 
 @app.route('/u/<username>/qr')
